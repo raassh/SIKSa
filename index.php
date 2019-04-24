ae05b1b2ba41caf66e6a540deaa712f05bc9242d
Andaresta Fauzan <andaresta.fauzan@gmail.com>
2019-04-24 23:44:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,15 @@
+<?php 
+include('koneksi.php');
+include('proseslogin.php');
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-    <!-- Bootstrap -->
-    <link href="css/bootstrap-4.0.0.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/home.css">
-
 	<title>Home</title>
 </head>
-<body style="margin-top: 100px; margin-left: 50px">
-	<section>
-  	<div>
-      <center>
-      		<div>
-        		<p>
-              <div class="btn btn-primary btn-lg" href="pilih_materi.html" role="button" style="width: 700px" ><img src="images/FieldNote.png" width="80" height="80" alt=""/>
-                Catat Pemasukan
-              </div>
-            </p>
-          </div>
-      <div>
-        		<p><div class="btn btn-primary btn-lg" href="pilih_materi.html" role="button" style="width: 700px"><img src="images/Calculator_icon.png" width="80" height="80" alt=""/> Catat Pengeluaran</div></p>
-      </div>
-
-      <div>
-        		<p><div class="btn btn-primary btn-lg" href="pilih_materi.html" role="button" style="width: 700px"><img src="images/graph.png" width="80" height="80" alt="" style="margin-left: 40px" />Lihat catatan Penjualan</div></p>
-      </div>
-
-      <div>
-            <p><div class="btn btn-primary btn-lg" href="'proseslogin.php?logout'" role="button" style="width: 700px"> LogOut</div></p>
-      </div>
-      </center>
-</div>
-    </section>
+<body>
+	<p>Hellow</p>
+	<li class="nav-item"> <a class="nav-link" href="proseslogin.php?logout">Logout</a></li>
 </body>
 </html>
